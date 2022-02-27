8b9cd17cddeabdc145a85d795303920caa04b0ed
Taemin1234 <86579707+Taemin1234@users.noreply.github.com>
2022-02-27 23:06:04 +0900

    Add files via upload

--- hopper.js
+++ hopper.js
@@ -11,32 +11,80 @@ $('.title').on('click',function(){
     $('html').scrollTop(0);
 })
 
+for(let i=0; i<".menu li".length; i++){
+    $(".menu li").eq(i).click(function() {
+            $('html').scrollTop($('.page-title').eq(i).offset().top)
+    })
+}
+
+
+$('.hopper').hover(
+    function() {
+         $('.hopper').animate({'left': 230},600)
+         $('.hopper-intro').animate({'left':800,'opacity': 1},600)
+    }
+)
 
 $(window).on('scroll',function(){
+
+    // navbar 크기 조절
     if ($(window).scrollTop() > 400) {
             $('.nav-bar').addClass('nav-bar-scr');
         } else {
             $('.nav-bar').removeClass('nav-bar-scr');
         }
 
-    if ($(window).scrollTop()>= 7900) {
+    
+    //대표작
+    //1865-2362 2578 - 2922
+
+    var y = -1/497*($(window).scrollTop()) + 2362/497
+    var x = -1/344*($(window).scrollTop()) + 1461/172
+    var z = -1/4970*($(window).scrollTop()) + 1367/994
+    var w = -1/3440*($(window).scrollTop()) + 3009/1720
+
+
+
+    $('.work-box').eq(0).css('opacity', y)//.css('transform',`scale(${z})`)
+
+    $('.work-box').eq(1).css('opacity', x)//.css('transform',`scale(${ww})`)
+
+    if($(window).scrollTop() > 1992) {
+        $('.work-box').eq(0).css('transform','scale(0.9)')
+    } else {
+        $('.work-box').eq(0).css('transform','scale(1)')
+    }
+
+    if($(window).scrollTop() > 2693) {
+        $('.work-box').eq(1).css('transform','scale(0.9)')
+    } else {
+        $('.work-box').eq(1).css('transform','scale(1)')
+    }
+
+
+    // 매체에서의 호퍼 박스
+    if ($(window).scrollTop()>= 4370) {
         $('.card-wrap').slice(0,2).css('top','0px')
     } else {
         $('.card-wrap').slice(0,2).css('top','150px')
     }
 
-    if ($(window).scrollTop()>= 8327) {
+    if ($(window).scrollTop()>= 4752) {
         $('.card-wrap').slice(2,4).css('top','0px')
     } else {
         $('.card-wrap').slice(2,4).css('top','150px')
     }
 
+    $(window).on('scroll',function(){
+        console.log($('html').scrollTop())
+    })
 });
 
-$('.card-wrap').on('click',function(){
-    $('.card-wrap').animate({'top' : 150},1000)
 
-})
+// $('.card-wrap').on('click',function(){
+//     $('.card-wrap').animate({'top' : 150},1000)
+
+// })
 
 // 스크롤에 따라 상단 메뉴바 숨기고 보이기
 
